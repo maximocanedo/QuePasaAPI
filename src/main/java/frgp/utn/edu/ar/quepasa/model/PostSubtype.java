f33e0e9e8dba0834b072bc1b64d599cf1f3712af
carlo-renosto <carlorenosto13@gmail.com>
2024-09-24 23:24:58 -0300

    Crear entidad PostSubtype #1

---
+++ src/main/java/frgp/utn/edu/ar/quepasa/model/PostSubtype.java
@@ -0,0 +1,40 @@
+package main.java.frgp.utn.edu.ar.quepasa.model;
+
+import frgp.utn.edu.ar.quepasa.model.User;
+import jakarta.persistence.*;
+
+/**
+ * Entidad que representa el subtipo de una publicación.
+ */
+@Entity
+@Table(name = "postSubtypes")
+public class PostSubtype {
+    private Integer id;
+    private PostType type;
+    private String description;
+
+    public PostSubtype() {}
+
+    /**
+     * Devuelve el ID único del subtipo de publicación.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    /**
+     * Devuelve el tipo de publicación asociado al subtipo de publicación.
+     */
+    @ManyToOne
+    @JoinColumn(name = "type_id", nullable = false)
+    public User getType() { return type; }  
+    public void setType(PostType type) { this.type = type; }
+
+    /**
+     * Devuelve la descripción del subtipo de publicación.
+     */
+    @Column(nullable = false)
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
