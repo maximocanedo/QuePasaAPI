2e37cdd5bdf83f7e9b07a1b64c1cbed0af90d425
carlo-renosto <carlorenosto13@gmail.com>
2024-09-24 23:05:47 -0300

    Crear entidad PostType #1

---
+++ src/main/java/frgp/utn/edu/ar/quepasa/model/PostType.java
@@ -0,0 +1,31 @@
+package main.java.frgp.utn.edu.ar.quepasa.model;
+
+import jakarta.persistence.*;
+
+/**
+ * Entidad que representa el tipo de una publicación.
+ */
+@Entity
+@Table(name = "posts")
+public class PostType {
+    private Integer id;
+    private String description;
+
+    public PostType() {}
+
+    /**
+     * Devuelve el ID único del tipo de publicación.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    /**
+     * Devuelve la descripción del tipo de publicación.
+     */
+    @Column(nullable = false)
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+}
