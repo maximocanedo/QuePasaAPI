0fef9b6df4070924e2a4510b46b4be63e118cc73
carlo-renosto <carlorenosto13@gmail.com>
2024-10-01 18:53:03 -0300

    Actualizar controlador PostController. Añadir validaciones de roles y usuarios #7

--- src/main/java/frgp/utn/edu/ar/quepasa/controller/PostController.java
+++ src/main/java/frgp/utn/edu/ar/quepasa/controller/PostController.java
@@ -1,24 +1,34 @@
 package frgp.utn.edu.ar.quepasa.controller;
 
+import frgp.utn.edu.ar.quepasa.data.request.post.PostCreateRequest;
 import frgp.utn.edu.ar.quepasa.data.request.post.PostPatchEditRequest;
+import frgp.utn.edu.ar.quepasa.model.User;
+import frgp.utn.edu.ar.quepasa.service.AuthenticationService;
 import frgp.utn.edu.ar.quepasa.service.PostService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
 
     private final PostService postService;
+    private final AuthenticationService authenticationService;
 
-    PostController(PostService postService) {
+    PostController(PostService postService, AuthenticationService authenticationService) {
         this.postService = postService;
+        this.authenticationService = authenticationService;
     }
 
     @PostMapping
     public ResponseEntity<?> createPost(@RequestBody PostCreateRequest post) {
-        return ResponseEntity.ok(postService.create(post));
+        User me = authenticationService.getCurrentUserOrDie();
+        return ResponseEntity.ok(postService.create(post, me));
     }
 
     @GetMapping("/all")
@@ -33,13 +43,15 @@ public class PostController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<?> updatePost(@PathVariable Integer id, @RequestBody PostPatchEditRequest post) {
-        return ResponseEntity.ok(postService.update(id, post));
+    public ResponseEntity<?> updatePost(@PathVariable Integer id, @RequestBody PostPatchEditRequest post) throws AccessDeniedException {
+        User me = authenticationService.getCurrentUserOrDie();
+        return ResponseEntity.ok(postService.update(id, post, me));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deletePost(@PathVariable Integer id) {
-        postService.delete(id);
+    public ResponseEntity<?> deletePost(@PathVariable Integer id) throws AccessDeniedException {
+        User me = authenticationService.getCurrentUserOrDie();
+        postService.delete(id, me);
         return ResponseEntity.ok(HttpStatus.NO_CONTENT);
     }
 
