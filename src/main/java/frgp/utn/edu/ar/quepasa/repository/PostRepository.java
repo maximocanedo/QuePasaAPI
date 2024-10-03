029ac40a1bc609cbce40a63b9c1d784b798ded9f
carlo-renosto <carlorenosto13@gmail.com>
2024-10-03 13:45:20 -0300

    Actualizar PostRepository. Añadir método findByOriginalPoster

--- src/main/java/frgp/utn/edu/ar/quepasa/repository/PostRepository.java
+++ src/main/java/frgp/utn/edu/ar/quepasa/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package frgp.utn.edu.ar.quepasa.repository;
 
 import frgp.utn.edu.ar.quepasa.model.Post;
+import frgp.utn.edu.ar.quepasa.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
-
+    Page<Post> findByOriginalPoster(User originalPoster, Pageable pageable);
 }
