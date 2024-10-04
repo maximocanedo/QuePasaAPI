e2aa90bc723db77eeee6f96844dd58df2f437622
carlo-renosto <carlorenosto13@gmail.com>
2024-10-04 01:48:34 -0300

    Actualizar servicio PostSubtypeService. Añadir metodo listPostSubtypes #73

--- src/main/java/frgp/utn/edu/ar/quepasa/service/PostSubtypeService.java
+++ src/main/java/frgp/utn/edu/ar/quepasa/service/PostSubtypeService.java
@@ -5,5 +5,5 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PostSubtypeService {
-
+    Page<PostSubtype> listPostSubtypes(Pageable pageable);
 }
--- src/main/java/frgp/utn/edu/ar/quepasa/service/impl/PostSubtypeServiceImpl.java
+++ src/main/java/frgp/utn/edu/ar/quepasa/service/impl/PostSubtypeServiceImpl.java
@@ -1,10 +1,19 @@
 package frgp.utn.edu.ar.quepasa.service.impl;
 
 import frgp.utn.edu.ar.quepasa.model.PostSubtype;
+import frgp.utn.edu.ar.quepasa.repository.PostSubtypeRepository;
 import frgp.utn.edu.ar.quepasa.service.PostSubtypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+@Service("postSubtypeService")
 public class PostSubtypeServiceImpl implements PostSubtypeService {
 
+    @Autowired
+    private PostSubtypeRepository postSubtypeRepository;
+
+    @Override
+    public Page<PostSubtype> listPostSubtypes(Pageable pageable) { return postSubtypeRepository.findAll(pageable); }
 }
