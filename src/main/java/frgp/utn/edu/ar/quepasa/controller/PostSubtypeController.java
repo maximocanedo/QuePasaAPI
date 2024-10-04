d481c2207afd9936d29151868f1426b979a38f6a
carlo-renosto <carlorenosto13@gmail.com>
2024-10-04 10:03:43 -0300

    Actualizar controlador PostSubtypeController. Añadir metodo create #72

--- src/main/java/frgp/utn/edu/ar/quepasa/controller/PostSubtypeController.java
+++ src/main/java/frgp/utn/edu/ar/quepasa/controller/PostSubtypeController.java
@@ -1,11 +1,17 @@
 package frgp.utn.edu.ar.quepasa.controller;
 
+import frgp.utn.edu.ar.quepasa.data.request.post.subtype.PostSubtypeRequest;
+import frgp.utn.edu.ar.quepasa.model.User;
 import frgp.utn.edu.ar.quepasa.service.AuthenticationService;
 import frgp.utn.edu.ar.quepasa.service.PostSubtypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequestMapping("/api/post-subtypes")
@@ -15,4 +21,9 @@ public class PostSubtypeController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @PostMapping
+    public ResponseEntity<?> createPostSubtype(@RequestBody PostSubtypeRequest subtype) throws AccessDeniedException {
+        User me = authenticationService.getCurrentUserOrDie();
+        return ResponseEntity.ok(postSubtypeService.create(subtype, me));
+    }
 }
