a7d999bb163f2a20b7a222e5546e94890fdf3f2c
carlo-renosto <carlorenosto13@gmail.com>
2024-10-04 02:04:36 -0300

    Actualizar repositorio PostSubtypeRepository. Añadir metodo findByPostType #74

--- src/main/java/frgp/utn/edu/ar/quepasa/repository/PostSubtypeRepository.java
+++ src/main/java/frgp/utn/edu/ar/quepasa/repository/PostSubtypeRepository.java
@@ -1,10 +1,13 @@
 package frgp.utn.edu.ar.quepasa.repository;
 
 import frgp.utn.edu.ar.quepasa.model.PostSubtype;
+import frgp.utn.edu.ar.quepasa.model.PostType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostSubtypeRepository extends JpaRepository<PostSubtype, Integer> {
-
+    Page<PostSubtype> findByPostType(PostType type, Pageable pageable);
 }
