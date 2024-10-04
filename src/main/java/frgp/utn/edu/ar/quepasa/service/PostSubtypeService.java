48123235be214d29f6f672ff8e7db3ba7132ebf8
carlo-renosto <carlorenosto13@gmail.com>
2024-10-04 01:39:02 -0300

    Crear servicio PostSubtypeService

---
+++ src/main/java/frgp/utn/edu/ar/quepasa/service/PostSubtypeService.java
@@ -0,0 +1,9 @@
+package frgp.utn.edu.ar.quepasa.service;
+
+import frgp.utn.edu.ar.quepasa.model.PostSubtype;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface PostSubtypeService {
+
+}
---
+++ src/main/java/frgp/utn/edu/ar/quepasa/service/impl/PostSubtypeServiceImpl.java
@@ -0,0 +1,10 @@
+package frgp.utn.edu.ar.quepasa.service.impl;
+
+import frgp.utn.edu.ar.quepasa.model.PostSubtype;
+import frgp.utn.edu.ar.quepasa.service.PostSubtypeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class PostSubtypeServiceImpl implements PostSubtypeService {
+
+}
