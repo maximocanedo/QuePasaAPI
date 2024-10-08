9c143ff779b48a0d309f3d9db191308837352590
carlo-renosto <carlorenosto13@gmail.com>
2024-10-08 15:30:32 -0300

    Actualizar test unitario PostTypeServiceTests. Añadir pruebas de consultas #68 #69

--- src/test/java/frgp/utn/edu/ar/quepasa/service/PostTypeServiceTests.java
+++ src/test/java/frgp/utn/edu/ar/quepasa/service/PostTypeServiceTests.java
@@ -12,8 +12,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.nio.file.AccessDeniedException;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -29,6 +34,50 @@ public class PostTypeServiceTests {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Buscar tipo de post por ID.")
+    void findById_TypeFound_ReturnsType() {
+        Integer id = 1;
+        PostType mockType = new PostType();
+        mockType.setId(id);
+
+        when(postTypeRepository.findById(id)).thenReturn(Optional.of(mockType));
+
+        PostType foundType = postTypeService.findById(id);
+        assertNotNull(foundType);
+        assertEquals(id, foundType.getId());
+    }
+
+    @Test
+    @DisplayName("Buscar tipo de post por ID, ID inexistente.")
+    void findById_TypeNotFound_ThrowsException() {
+        Integer id = 1;
+
+        when(postTypeRepository.findById(id)).thenReturn(Optional.empty());
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            postTypeService.findById(id);
+        });
+
+        assertEquals("Type not found", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Buscar tipos de post.")
+    void findTypes_TypesFound_ReturnsTypes() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        PostType type = new PostType();
+        Page<PostType> typePage = new PageImpl<>(List.of(type));
+
+        when(postTypeRepository.findAll(pageable)).thenReturn(typePage);
+
+        Page<PostType> types = postTypeRepository.findAll(pageable);
+
+        assertNotNull(types);
+        assertFalse(types.isEmpty());
     }
 
     @Test
