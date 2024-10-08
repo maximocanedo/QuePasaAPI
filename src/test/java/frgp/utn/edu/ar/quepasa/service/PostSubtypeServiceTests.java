2b36bc56ebf091e2b6493728ac07fae8eb5c848c
carlo-renosto <carlorenosto13@gmail.com>
2024-10-08 18:55:05 -0300

    Actualizar test unitario PostSubtypeServiceTests. Añadir pruebas de consultas #73 #74

--- src/test/java/frgp/utn/edu/ar/quepasa/service/PostSubtypeServiceTests.java
+++ src/test/java/frgp/utn/edu/ar/quepasa/service/PostSubtypeServiceTests.java
@@ -15,8 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.nio.file.AccessDeniedException;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -33,6 +38,94 @@ public class PostSubtypeServiceTests {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Buscar subtipo de post por ID.")
+    void findById_SubtypeFound_ReturnsSubtype() {
+        Integer id = 1;
+        PostSubtype mockSubtype = new PostSubtype();
+        mockSubtype.setId(id);
+
+        when(postSubtypeRepository.findById(id)).thenReturn(Optional.of(mockSubtype));
+
+        PostSubtype foundSubtype = postSubtypeService.findById(id);
+        assertNotNull(foundSubtype);
+        assertEquals(id, foundSubtype.getId());
+    }
+
+    @Test
+    @DisplayName("Buscar subtipo de post por ID, ID inexistente.")
+    void findById_SubtypeNotFound_ThrowsException() {
+        Integer id = 1;
+
+        when(postSubtypeRepository.findById(id)).thenReturn(Optional.empty());
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            postSubtypeService.findById(id);
+        });
+
+        assertEquals("Subtype not found", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Buscar subtipos de post.")
+    void findSubtypes_SubtypesFound_ReturnsSubtypes() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        PostSubtype subtype = new PostSubtype();
+        Page<PostSubtype> subtypePage = new PageImpl<>(List.of(subtype));
+
+        when(postSubtypeRepository.findAll(pageable)).thenReturn(subtypePage);
+
+        Page<PostSubtype> subtypes = postSubtypeService.listPostSubtypes(pageable);
+
+        assertNotNull(subtypes);
+        assertFalse(subtypes.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Buscar subtipos de post por tipo.")
+    void findByType_TypeFound_ReturnsSubtypes() {
+        Integer id = 1;
+        Integer idType = 4;
+        Pageable pageable = PageRequest.of(0, 10);
+        PostSubtype mockSubtype = new PostSubtype();
+        mockSubtype.setId(id);
+
+        when(postSubtypeRepository.findById(id)).thenReturn(Optional.of(mockSubtype));
+
+        PostType mockType = new PostType();
+        mockType.setId(idType);
+
+        when(postTypeRepository.findById(idType)).thenReturn(Optional.of(mockType));
+
+        mockSubtype.setType(mockType);
+        Page<PostSubtype> subtypePage = new PageImpl<>(List.of(mockSubtype));
+
+        when(postSubtypeRepository.findByType(mockType, pageable)).thenReturn(subtypePage);
+
+        Page<PostSubtype> subtypes = postSubtypeService.findByType(idType, pageable);
+        assertNotNull(subtypes);
+        assertFalse(subtypes.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Buscar subtipos de post por tipo, tipo no existente.")
+    void findByType_TypeNotFound_ThrowsException() {
+        Integer idType = 4;
+        Pageable pageable = PageRequest.of(0, 10);
+
+        PostType mockType = new PostType();
+        mockType.setId(idType);
+
+        when(postTypeRepository.findById(idType)).thenReturn(Optional.empty());
+
+        when(postSubtypeRepository.findByType(mockType, pageable)).thenReturn(Page.empty());
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            postSubtypeService.findByType(idType, pageable);
+        });
     }
 
     @Test
