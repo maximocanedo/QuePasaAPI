0ffba95d9d6a2f102119f161b8f2b958d259de1c
lucas <lucasgruden0@protonmail.com>
2024-10-08 17:21:15 -0300

    Test obtener evento por ID #93

--- src/test/java/frgp/utn/edu/ar/quepasa/service/EventServiceTest.java
+++ src/test/java/frgp/utn/edu/ar/quepasa/service/EventServiceTest.java
@@ -8,6 +8,7 @@ import frgp.utn.edu.ar.quepasa.service.impl.EventServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
@@ -15,35 +16,59 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 public class EventServiceTest {
-    @Mock private EventServiceImpl eventService;
     @Mock private EventRepository eventRepository;
     @Mock private EventRsvpRepository eventRsvpRepository;
     @Mock private NeighbourhoodRepository neighbourhoodRepository;
+    @InjectMocks private EventServiceImpl eventService;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
 
+
     @Test
     @DisplayName("Obtener Todos los Eventos Activos")
     void findAllEvents_EventsFound_ReturnAllEvents() {
         Pageable pageable = PageRequest.of(0, 10);
 
-        Event event = new Event();
-        Page<Event> mockPage = new PageImpl<>(List.of(event));
+        Event event1 = new Event();
+        event1.setActive(true);
+        Event event2 = new Event();
+        event2.setActive(true);
 
+        Page<Event> mockPage = new PageImpl<>(Arrays.asList(event1, event2));
         when(eventRepository.search("", pageable, true)).thenReturn(mockPage);
 
         Page<Event> events = eventService.getEvents("", pageable, true);
 
-        //assertNotNull(events);
+        assertNotNull(events);
         assertFalse(events.isEmpty());
+        assertEquals(2, events.getTotalElements());
+        assertTrue(events.getContent().get(0).isActive());
+        assertTrue(events.getContent().get(1).isActive());
+    }
+
+    @Test
+    @DisplayName("Obtener Evento por ID")
+    void findEventById_EventFound_ReturnEvent() {
+        UUID eventId = UUID.randomUUID();
+        Event event = new Event();
+        event.setId(eventId);
+
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+
+        Event foundEvent = eventService.findById(eventId);
+
+        assertNotNull(foundEvent);
+        assertEquals(eventId, foundEvent.getId());
     }
 }
