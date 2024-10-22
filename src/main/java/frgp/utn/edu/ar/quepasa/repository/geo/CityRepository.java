71888996b121f11efb2367f9029ca630edd74864
carlo-renosto <carlorenosto13@gmail.com>
2024-10-21 23:42:04 -0300

    Actualizar CityRepository. Corregir método search #43

--- src/main/java/frgp/utn/edu/ar/quepasa/repository/geo/CityRepository.java
+++ src/main/java/frgp/utn/edu/ar/quepasa/repository/geo/CityRepository.java
@@ -1,6 +1,7 @@
 package frgp.utn.edu.ar.quepasa.repository.geo;
 
 import frgp.utn.edu.ar.quepasa.model.geo.City;
+import frgp.utn.edu.ar.quepasa.model.geo.SubnationalDivision;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,8 +9,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
-    @Query("SELECT e FROM Event e WHERE (e.name LIKE %:query% OR e.subdivision LIKE %:query%) AND e.active = :active")
+    @Query("SELECT c FROM City c WHERE (c.name LIKE %:query% OR c.subdivision.iso3 LIKE %:query%) AND c.active = :active")
     Page<City> search(@NotNull String query, @NotNull Pageable pageable, boolean active);
+
+    List<City> findBySubdivision(SubnationalDivision subdivision);
 }
