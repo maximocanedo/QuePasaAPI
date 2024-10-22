7767577e5a69c41fd44130024519e4acfee7180f
lucas <lucasgruden0@protonmail.com>
2024-10-22 17:19:26 -0300

    Tests #102 #97 #96

--- src/test/java/frgp/utn/edu/ar/quepasa/service/EventServiceTest.java
+++ src/test/java/frgp/utn/edu/ar/quepasa/service/EventServiceTest.java
@@ -517,6 +517,47 @@ public class EventServiceTest {
     }
 
     @Test
+    @DisplayName("Actualizar Evento Permisos Insuficientes")
+    void updateEvent_InsufficientPermissions_ThrowFail() {
+        UUID eventId = UUID.randomUUID();
+        String username = "owner";
+        String username2 = "user";
+
+        User owner = new User();
+        owner.setUsername(username);
+        owner.setRole(Role.USER);
+
+        User user = new User();
+        user.setUsername(username2);
+        user.setRole(Role.USER);
+
+        Event event = new Event();
+        event.setId(eventId);
+        event.setOwner(user);
+
+        setAuthContext(username, "USER");
+
+        when(authenticationService.getCurrentUserOrDie()).thenReturn(owner);
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(owner));
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+
+        EventPatchEditRequest eventPatchEditRequest = new EventPatchEditRequest();
+        eventPatchEditRequest.setTitle("event");
+        eventPatchEditRequest.setDescription("description");
+        eventPatchEditRequest.setAddress("address");
+        eventPatchEditRequest.setCategory(EventCategory.CINEMA);
+        eventPatchEditRequest.setAudience(Audience.PUBLIC);
+
+        Fail exception = assertThrows(
+                Fail.class,
+                () -> eventService.update(eventId, eventPatchEditRequest, user)
+        );
+
+        assertTrue(exception.getMessage().contains("Error de accesos."));
+        clearAuthContext();
+    }
+
+    @Test
     @DisplayName("Eliminar Evento")
     void deleteEvent_ValidEvent_ReturnEvent() {
         UUID eventId = UUID.randomUUID();
@@ -568,6 +609,40 @@ public class EventServiceTest {
     }
 
     @Test
+    @DisplayName("Eliminar Evento Permisos Insuficientes")
+    void deleteEvent_InsufficientPermissions_ThrowFail() {
+        UUID eventId = UUID.randomUUID();
+        String username = "owner";
+        String username2 = "user";
+
+        User owner = new User();
+        owner.setUsername(username);
+        owner.setRole(Role.USER);
+
+        User user = new User();
+        user.setUsername(username2);
+        user.setRole(Role.USER);
+
+        Event event = new Event();
+        event.setId(eventId);
+        event.setOwner(user);
+
+        setAuthContext(username, "USER");
+
+        when(authenticationService.getCurrentUserOrDie()).thenReturn(owner);
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(owner));
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+
+        Fail exception = assertThrows(
+                Fail.class,
+                () -> eventService.delete(eventId)
+        );
+
+        assertTrue(exception.getMessage().contains("Error de accesos."));
+        clearAuthContext();
+    }
+
+    @Test
     @DisplayName("Confirmar Asistencia a Evento")
     void confirmEventAssistance_ValidEvent_ReturnEventRsvp() {
         UUID eventId = UUID.randomUUID();
@@ -609,6 +684,29 @@ public class EventServiceTest {
         );
 
         assertEquals("Event not found.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Confirmar Asistencia a Evento Inactivo")
+    void confirmEventAssistance_EventInactive_ThrowFail() {
+        UUID eventId = UUID.randomUUID();
+        User user = new User();
+        user.setUsername("user");
+
+        Event event = new Event();
+        event.setId(eventId);
+        event.setActive(false);
+
+        setAuthContext(user.getUsername(), "USER");
+
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+
+        Fail exception = assertThrows(
+                Fail.class,
+                () -> eventService.confirmEventAssistance(eventId, user)
+        );
+
+        assertTrue(exception.getMessage().contains("Event is not active."));
     }
 
     @Test
