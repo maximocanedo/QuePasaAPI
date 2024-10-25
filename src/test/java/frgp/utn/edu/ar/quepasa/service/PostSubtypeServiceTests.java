0d308d82583a339308f552f3fa9e0ae5d98fc3aa
carlo-renosto <carlorenosto13@gmail.com>
2024-10-25 14:26:45 -0300

    Actualizar PostSubtypeServiceTests. Corregir llamado a método findAllActive #74

--- src/test/java/frgp/utn/edu/ar/quepasa/service/PostSubtypeServiceTests.java
+++ src/test/java/frgp/utn/edu/ar/quepasa/service/PostSubtypeServiceTests.java
@@ -82,7 +82,7 @@ public class PostSubtypeServiceTests {
         PostSubtype subtype = new PostSubtype();
         Page<PostSubtype> subtypePage = new PageImpl<>(List.of(subtype));
 
-        when(postSubtypeRepository.findAll(pageable)).thenReturn(subtypePage);
+        when(postSubtypeRepository.findAllActive(pageable)).thenReturn(subtypePage);
 
         Page<PostSubtype> subtypes = postSubtypeService.findAll(pageable, true);
 
