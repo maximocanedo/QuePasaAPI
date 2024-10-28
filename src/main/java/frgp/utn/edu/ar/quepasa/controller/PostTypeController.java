a12a181647891f9d965f94d6d69856f7ab2b1a5b
carlo-renosto <carlorenosto13@gmail.com>
2024-10-28 20:16:26 -0300

    Documentación clases PostType #114

--- src/main/java/frgp/utn/edu/ar/quepasa/controller/PostTypeController.java
+++ src/main/java/frgp/utn/edu/ar/quepasa/controller/PostTypeController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -27,17 +28,41 @@ public class PostTypeController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * Crea un tipo nuevo.
+     *
+     * @param description Descripción del tipo a crear.
+     * @return Entidad de respuesta con los detalles del tipo creado.
+     */
     @PostMapping
     public ResponseEntity<?> createPostType(@RequestBody String description) {
         return ResponseEntity.ok(postTypeService.create(description));
     }
 
+    /**
+     * Obtiene una lista paginada de tipos activos o inactivos, según sea especificado.
+     *
+     * @param page Número de páginas a obtener. Comienza en 0.
+     * @param size Tamaño de cada página. Comienza en 10.
+     * @param activeOnly Si se desean obtener solo los tipos activos. Valor predeterminado es true.
+     * @return Entidad de respuesta con una lista paginada de tipos encontrados.
+     */
     @GetMapping("/all")
     public ResponseEntity<?> getPostTypes(@RequestParam(defaultValue="0") int page, @RequestParam(defaultValue="10") int size,  @RequestParam(defaultValue="true") boolean activeOnly) {
         Pageable pageable = PageRequest.of(page, size);
         return ResponseEntity.ok(postTypeService.findAll(pageable, activeOnly));
     }
 
+    /**
+     * Obtiene una lista paginada de tipos que coinciden con los criterios de búsqueda especificados.
+     *
+     * @param q Parámetro de búsqueda que se usará para filtrar los tipos.
+     * @param sort Parámetro de ordenamiento para los tipos, con un valor predeterminado de "title,asc".
+     * @param page Número de páginas a obtener. Comienza en 0.
+     * @param size Tamaño de cada página. Comienza en 10.
+     * @param active Si se desean obtener solo los tipos activos. Valor predeterminado es true.
+     * @return Entidad de respuesta con una lista paginada de tipos filtrados.
+     */
     @GetMapping("/search")
     public ResponseEntity<?> getPostTypes(@RequestParam(defaultValue="") String q, @RequestParam(defaultValue="description,asc") String sort, @RequestParam(defaultValue="0") int page, @RequestParam(defaultValue="10") int size, @RequestParam(defaultValue="true") boolean active) {
         Sort.Direction direction = Sort.Direction.ASC;
@@ -48,21 +73,36 @@ public class PostTypeController {
         return ResponseEntity.ok(postTypeService.search(q, pageable, active));
     }
 
+    /**
+     * Obtiene un tipo según su ID.
+     *
+     * @param id ID del tipo a buscar.
+     * @return Entidad de respuesta que contiene el tipo buscado.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> getPostTypeById(@PathVariable Integer id) {
         return ResponseEntity.ok(postTypeService.findById(id));
     }
 
+    /**
+     * Edita un tipo.
+     * @param id ID del tipo a editar.
+     * @return Entidad de respuesta que contiene el tipo editado.
+     */
     @PatchMapping("/{id}")
     public ResponseEntity<?> updatePostType(@PathVariable Integer id, @RequestBody String description) {
         return ResponseEntity.ok(postTypeService.update(id, description));
     }
 
+    /**
+     * Elimina un tipo.
+     * @param id ID del tipo a eliminar.
+     * @return Entidad de respuesta de tipo 204 (No Content).
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletePostType(@PathVariable Integer id) {
         postTypeService.delete(id);
         return ResponseEntity.ok(HttpStatus.NO_CONTENT);
     }
-
 
 }
--- src/main/java/frgp/utn/edu/ar/quepasa/service/impl/PostTypeServiceImpl.java
+++ src/main/java/frgp/utn/edu/ar/quepasa/service/impl/PostTypeServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.AccessDeniedException;
+
 @Service("postTypeService")
 public class PostTypeServiceImpl implements PostTypeService {
 
@@ -20,11 +22,29 @@ public class PostTypeServiceImpl implements PostTypeService {
         this.postTypeRepository = postTypeRepository;
     }
 
+    /**
+     * Busca todos los tipos que contengan el texto dado en la descripción,
+     * activos o no activos, de acuerdo al parámetro dado.
+     * La lista se devuelve paginada según el objeto Pageable dado.
+     *
+     * @param q El texto que se busca en el título y descripción de las publicaciones
+     * @param pageable El objeto que contiene la información de la paginación
+     * @param active Indica si se quieren obtener solo los tipos activos o no
+     * @return La lista de tipos paginada
+     */
     @Override
     public Page<PostType> search(String q, Pageable pageable, boolean active) {
         return postTypeRepository.search(q, pageable, active);
     }
 
+    /**
+     * Busca todos los tipos, activos o no activos, de acuerdo al parámetro dado.
+     * La lista se devuelve paginada según el objeto Pageable dado.
+     *
+     * @param pageable El objeto que contiene la información de la paginación
+     * @param activeOnly Indica si se quieren obtener solo los tipos activas o no
+     * @return La lista de tipos paginada
+     */
     @Override
     public Page<PostType> findAll(Pageable pageable, boolean activeOnly) {
         if(activeOnly) {
@@ -33,12 +53,27 @@ public class PostTypeServiceImpl implements PostTypeService {
         return postTypeRepository.findAll(pageable);
     }
 
+    /**
+     * Busca un tipo por su ID y lo devuelve.
+     *
+     * @param id El ID del tipo que se busca
+     * @return El tipo encontrado
+     * @throws Fail Si el tipo no es encontrado
+     */
     @Override
     public PostType findById(Integer id) {
         return postTypeRepository.findById(id)
                 .orElseThrow(() -> new Fail("Type not found", HttpStatus.NOT_FOUND));
     }
 
+    /**
+     * Crea un nuevo tipo.
+     *
+     * El usuario actual debe ser administrador.
+     *
+     * @param description La descripción del nuevo tipo
+     * @return El tipo creado
+     */
     @Override
     public PostType create(String description) {
         PostType type = new PostType();
@@ -47,6 +82,15 @@ public class PostTypeServiceImpl implements PostTypeService {
         return type;
     }
 
+    /**
+     * Actualiza un tipo.
+     *
+     * El usuario actual debe ser administrador.
+     *
+     * @param id El ID del tipo
+     * @param description La descripción del tipo
+     * @return El tipo actualizado
+     */
     @Override
     public PostType update(Integer id, String description) {
         PostType type = findById(id);
@@ -55,6 +99,13 @@ public class PostTypeServiceImpl implements PostTypeService {
         return type;
     }
 
+    /**
+     * Elimina un tipo.
+     *
+     * Sólo un administrador puede eliminarlo.
+     *
+     * @param id El ID del tipo a eliminar
+     */
     @Override
     public void delete(Integer id) {
         PostType type = findById(id);
